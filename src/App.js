93ea559e08b79600391c76ac717a515b48d70121
AEliu <95649311@qq.com>
2022-10-06 18:08:21 +0800

    useMatch

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import {
   Routes,
   Route,
   Link,
+  Navigate,
   useParams,
   useNavigate,
+  redirect,
+  useMatch,
 } from "react-router-dom";
 
 const Approvalform = ({
@@ -34,7 +37,7 @@ const Approvalform = ({
       heading: "",
       receiver: "",
     });
-    navigate(`/approvalsheet/sheets/${approvalObject.id}`)
+    navigate(`/approvalsheet/sheets/${approvalObject.id}`);
   };
 
   const timestamp = () => Date.now();
@@ -74,23 +77,20 @@ const Approvalform = ({
           Submit
         </button>
       </form>
-      
+
       <Link to="/approvalsheet/sheets" style={padding}>
         发文历史
       </Link>
-
     </>
   );
 };
 
-const Approval = ({ approvals }) => {
-  let id = useParams().id;
-  const approval = approvals.find((n) => n.id === Number(id));
-  let t = new Date(approval.id);
-  let date = t.toLocaleDateString();
+const Approval = ({ sheet }) => {
+  const t = new Date(sheet.id);
+  const date = t.toLocaleDateString();
   return (
     <li>
-      {date}-{approval.heading}
+      {date}-{sheet.heading}
     </li>
   );
 };
@@ -100,20 +100,31 @@ const Sheets = ({ sheets }) => {
     padding: 5,
   };
   return (
-  <div>
-    <h2>Notes</h2>
-    <ul>
-      {sheets.map((sheet) => (
-        <li key={sheet.id}>
-          <Link to={`/approvalsheet/sheets/${sheet.id}`}>{sheet.heading}</Link>
-        </li>
-      ))}
-    </ul>
-    <Link to="/approvalsheet/" style={padding}>
+    <div>
+      <h2>Notes</h2>
+      <ul>
+        {sheets.map((sheet) => (
+          <li key={sheet.id}>
+            <Link to={`/approvalsheet/sheets/${sheet.id}`}>
+              {sheet.heading}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Link to="/approvalsheet/" style={padding}>
         HOME
       </Link>
-  </div>
-)};
+    </div>
+  );
+};
+
+const PageNotFound = () => {
+  return (
+    <>
+      <h2>404 Page not found</h2>
+    </>
+  );
+};
 
 const App = () => {
   const [approvals, setApprovals] = useState([]);
@@ -128,17 +139,15 @@ const App = () => {
     if (allApprovals) setApprovals(allApprovals);
   }, []);
 
-  
+  const match = useMatch("/approvalsheet/sheets/:id")
+  const sheet = match ? approvals.find((n) => n.id === Number(match.params.id))
+    : null
+
 
   return (
     <>
-      <Router>
-        {/* <Link to="/approvalsheet/sheets" style={padding}>
-          HISTORY
-        </Link> */}
-        {/* <Link to="/approvalsheet/" style={padding}>
-          HOME
-        </Link> */}
+      
+
         <Routes>
           <Route
             path="/approvalsheet/sheets"
@@ -158,21 +167,17 @@ const App = () => {
           />
           <Route
             path="/approvalsheet/sheets/:id"
-            element={<Approval approvals={approvals} />}
+            element={<Approval sheet={sheet} />}
+          />
+          <Route path="/approvalsheet/404" element={<PageNotFound />} />
+          <Route
+            path="*"
+            element={<Navigate to="/approvalsheet/404" replace />}
           />
         </Routes>
-      </Router>
+      
 
       <br />
-
-      {/* <ul>
-        {approvals
-          .slice(-10)
-          .reverse()
-          .map((approval) => (
-            <Approval key={approval.id.toString()} approval={approval} />
-          ))}
-      </ul> */}
     </>
   );
 };
